d1b1219a42cfb757693e8f7aad927fd2433dac7e
sashapff <a.ivanova@niuitmo.ru>
2021-10-22 16:45:24 +0300

    Add asserts

--- hw1/src/LRUCache.h
+++ hw1/src/LRUCache.h
@@ -11,27 +11,35 @@
 
 class LRUCache {
 public:
-    LRUCache(size_t max_size) : max_size_(max_size) {
+    explicit LRUCache(size_t max_size) : max_size_(max_size) {
+        assert(max_size_ > 0);
     }
 
     void Set(const std::string &key, const std::string &value) {
+        assert(size_ <= max_size_);
+        assert(map_.size() <= max_size_);
         if (map_.find(key) != map_.end()) {
-            UpdatePosition(key);
-            Add(key, value);
-            return;
+            RemoveKey(key);
+            AddKey(key, value);
+        } else {
+            if (size_ == max_size_) {
+                DropLast();
+            }
+            AddKey(key, value);
         }
-        if (size_ == max_size_) {
-            DropLast();
-        }
-        Add(key, value);
-        size_++;
+        assert(size_ <= max_size_);
+        assert(map_.size() <= max_size_);
     }
 
     bool Get(const std::string &key, std::string *value) {
+        assert(size_ <= max_size_);
+        assert(map_.size() <= max_size_);
         if (map_.find(key) != map_.end()) {
             *value = map_[key];
-            UpdatePosition(key);
-            Add(key, *value);
+            RemoveKey(key);
+            AddKey(key, *value);
+            assert(size_ <= max_size_);
+            assert(map_.size() <= max_size_);
             return true;
         }
         return false;
@@ -45,23 +53,55 @@ private:
     std::list<std::string> list_;
 
     void DropLast() {
+        assert(size_ == max_size_);
+        assert(map_.size() == max_size_);
+
         std::string key = list_.front();
         map_.erase(key);
         pointers_.erase(key);
         list_.pop_front();
         size_--;
+
+        assert(map_.find(key) == map_.end());
+        assert(pointers_.find(key) == pointers_.end());
+        assert(size_ + 1 == max_size_);
+        assert(map_.size() + 1 == max_size_);
     }
 
-    void Add(const std::string &key, const std::string &value) {
+    void AddKey(const std::string &key, const std::string &value) {
+        assert(size_ < max_size_);
+        assert(map_.size() < max_size_);
+
         map_[key] = value;
         list_.push_back(key);
         pointers_[key] = list_.end();
         --pointers_[key];
+        size_++;
+
+        assert(size_ <= max_size_);
+        assert(map_.size() <= max_size_);
+        auto last = --list_.end();
+        assert(*last == key);
+        assert(map_[*last] == value);
+        assert(map_.find(key)->second == value);
+        assert(pointers_.find(key) != pointers_.end());
     }
 
-    void UpdatePosition(const std::string &key) {
+    void RemoveKey(const std::string &key) {
+        assert(size_ <= max_size_);
+        assert(map_.size() <= max_size_);
+
         auto position = pointers_[key];
         list_.erase(position);
+        pointers_.erase(key);
+        map_.erase(key);
+        size_--;
+
+        assert(size_ < max_size_);
+        assert(map_.size() < max_size_);
+        assert(map_.find(key) == map_.end());
+        assert(pointers_.find(key) == pointers_.end());
+
     }
 };
 
