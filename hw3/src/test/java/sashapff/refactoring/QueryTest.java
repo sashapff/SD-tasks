f9de4e86057647062a4d673e7745c4bea3ca497c
sashapff <a.ivanova@niuitmo.ru>
2021-10-23 00:04:31 +0300

    Add tests for QueryServlet

--- hw3/src/test/java/sashapff/refactoring/QueryTest.java
+++ hw3/src/test/java/sashapff/refactoring/QueryTest.java
@@ -1,4 +1,34 @@
 package sashapff.refactoring;
 
+import org.junit.Test;
+import org.mockito.Mockito;
+import sashapff.refactoring.servlet.GetProductsServlet;
+import sashapff.refactoring.servlet.QueryServlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 public class QueryTest {
+
+    @Test
+    public void testDoGet() throws IOException {
+        QueryServlet servlet = new QueryServlet();
+
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+
+        Mockito.when(request.getParameter("command")).thenReturn("max");
+        Mockito.when(response.getWriter()).thenReturn(printWriter);
+        Mockito.clearInvocations(response);
+
+        servlet.doGet(request, response);
+
+        Mockito.verify(response).setContentType("text/html");
+        Mockito.verify(response).setStatus(HttpServletResponse.SC_OK);
+    }
 }
