a35c4e7b210282ff46583832b6db6640a457cbc5
sashapff <a.ivanova@niuitmo.ru>
2021-10-22 14:27:22 +0300

    Remove build files

--- hw1/cmake-build-debug/lib/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sashapff/itmo/SD-tasks/hw1/lib/googletest
-# Build directory: /Users/sashapff/itmo/SD-tasks/hw1/cmake-build-debug/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
